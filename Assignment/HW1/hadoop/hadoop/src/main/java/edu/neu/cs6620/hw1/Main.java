14c9d211a73ecffe375d82ff46ad271dc7ee7568
jasonhaoyuanmscs <yuan.hao3@northeastern.edu>
2024-09-30 23:12:17 -0700

    started

--- Assignment/HW1/hadoop/hadoop/src/main/java/edu/neu/cs6620/hw1/Main.java
+++ Assignment/HW1/hadoop/hadoop/src/main/java/edu/neu/cs6620/hw1/Main.java
@@ -2,6 +2,16 @@ package edu.neu.cs6620.hw1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            String fileLocation = args[0];
+            int clusterCount = Integer.parseInt(args[1]);
+            System.out.println("Application started with parameters: ");
+            System.out.println("File location: "+fileLocation);
+            System.out.println("Cluster count: "+clusterCount);
+        }catch (Exception e){
+            System.out.println("Application failed with cause: " + e.getMessage());
+            System.exit(1);
+        }
+
     }
 }
